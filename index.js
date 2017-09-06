c85f218ebed2a33005f31bb72b7ddd0e9d0c0e98
kumudug <kumudug1@gmail.com>
2017-09-06 10:50:30 +1000

    setup simple ordering code

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 let prods = getProducts();
 console.log(prods);
 
-let productsHtml = "<form>";
+let productsHtml = "";
 for (num = 0; num < prods.length; num++) {
-    productsHtml +=
-        "<div class='form-group'>" +
-        "<label for='inputId" + num + "'>Id</label>" +
-        "<label type='text' class='form-control' id='inputId" + num + "'>" + prods[num].id + "</label>" +
-        "</div>" +
-
-        "<div class='form-group'>" +
-        "<label for='inputName" + num + "'>Name</label>" +
-        "<label type='text' class='form-control' id='inputName" + num + "'>" + prods[num].name + "</label>" +
-        "</div>" +
-
-        "<div class='form-group'>" +
-        "<label for='inputDesc" + num + "'>Description</label>" +
-        "<label type='text' class='form-control' id='inputDesc" + num + "'>" + prods[num].description + "</label>" +
-        "</div>" +
-
-        "<div class='form-group'>" +
-        "<label for='inputPrice" + num + "'>Price</label>" +
-        "<label type='text' class='form-control' id='inputPrice" + num + "'>" + prods[num].price + "</label>" +
-        "</div>" +
-
-        "<hr/>";
+    productsHtml +=`<div type="button" class="list-group-item">${prods[num].name}<button type="button" class="btn btn-xs pull-right" onclick="addProdToOrder(${prods[num].id})" >Add</button><input type="number" name="quantity" min="1" max="20" class="pull-right"></div>`;
 }
-productsHtml += "</form>";
 
-document.getElementById('divProductsLarge').innerHTML = productsHtml;
-document.getElementById('divProductsSmall').innerHTML = productsHtml;
+document.getElementById('containerProducts').innerHTML = productsHtml;
 
+let customerList = getCustomers();
 
- 
+let customersHtml = '';
+for (num = 0; num < customerList.length; num++) {
+    customersHtml += `<div type="button" class="list-group-item">${customerList[num].name}</div>`;
+}
+document.getElementById('containerCustomers').innerHTML = customersHtml;
+
+ function addProdToOrder(prodId){
+     console.log(prodId);
+     addProduct(prodId, 1);
+     displayOrder();
+ }
+
+ let orderHtml = '';
+ function displayOrder(){
+    orderHtml = '';
+    for (num = 0; num < order.length; num++) {
+        orderHtml += `<div type="button" class="list-group-item">ProductId:${order[num].productId}, Qty: ${order[num].quantity}</div>`;
+    }
+    document.getElementById('containerOrder').innerHTML = orderHtml;
+ }
